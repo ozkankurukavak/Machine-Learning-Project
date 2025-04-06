0c92ea15b7b59d19c6eea0c4f39d67f2bb8b5e2f
özkan kurukavak <158309837+ozkankurukavak@users.noreply.github.com>
2025-04-06 10:44:44 +0000

    training.pipeline

--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -8,27 +8,33 @@ from us_visa.entity.artifact_entity import DataIngestionArtifact
 
 class TrainPipeline:
     def __init__(self):
+        # Veri çekme konfigürasyonunu başlatıyoruz
         self.data_ingestion_config = DataIngestionConfig()
-
-    
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
-        This method of TrainPipeline class is responsible for starting data ingestion component
+        Bu metot, TrainPipeline sınıfının veri çekme (data ingestion) bileşenini başlatmaktan sorumludur.
         """
         try:
-            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-            logging.info("Getting the data from mongodb")
+            # Metoda girildiğini logluyoruz
+            logging.info("TrainPipeline sınıfının start_data_ingestion metoduna girildi.")
+            logging.info("Veriler MongoDB'den alınıyor.")
+            
+            # Veri çekme işlemi için DataIngestion sınıfından bir nesne oluşturuluyor
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            # Veri çekme işlemi başlatılıyor ve sonuç alınıyor
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train_set and test_set from mongodb")
-            logging.info(
-                "Exited the start_data_ingestion method of TrainPipeline class"
-            )
+            
+            # Eğitim ve test verileri MongoDB'den alındı loglanıyor
+            logging.info("Eğitim verisi (train_set) ve test verisi (test_set) MongoDB'den alındı.")
+            # Metodun sonlandırıldığı loglanıyor
+            logging.info("start_data_ingestion metodundan çıkıldı.")
+            
+            # Veri çekme işleminin sonucu (data_ingestion_artifact) döndürülüyor
             return data_ingestion_artifact
         except Exception as e:
+            # Hata oluşursa, özel bir hata sınıfı olan USvisaException ile hata fırlatılıyor
             raise USvisaException(e, sys) from e
-        
 
     def run_pipeline(self, ) -> None:
         """
